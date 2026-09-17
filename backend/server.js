27cdb7b70bd9b1d3f1efb099c0ffd4f4b032d5a1
sreevishnu820 <sreevishnu820@gmail.com>
2026-09-17 11:24:35 +0530

    dbms project

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
-// server.js
 const express = require('express');
 const cors = require('cors');
+const path = require('path'); // Added the path module
 const { classifySQL } = require('./classifier');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// Tell Node to serve your frontend folder as a web page
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 app.post('/classify', (req, res) => {
     const { sql } = req.body;
